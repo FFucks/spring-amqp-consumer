a1540d607cc041f7de2aa74fc630bbf916254262
FFucks <fabio_geiss@hotmail.com>
2026-02-02 14:30:18 -0300

    Updated consumer

--- src/main/java/com/ffucks/configs/RabbitConfig.java
+++ src/main/java/com/ffucks/configs/RabbitConfig.java
@@ -1,34 +1,8 @@
 package com.ffucks.configs;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-
     public static final String QUEUE = "hello.queue";
-    public static final String EXCHANGE = "hello.exchange";
-    public static final String ROUTING_KEY = "hello.key";
-
-    @Bean
-    Queue queue() {
-        return new Queue(QUEUE, false);
-    }
-
-    @Bean
-    DirectExchange exchange() {
-        return new DirectExchange(EXCHANGE);
-    }
-
-    @Bean
-    Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue)
-                .to(exchange)
-                .with(ROUTING_KEY);
-    }
-
 }
